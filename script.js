2cbff7be202d1f460649c4815d1f86ce79e89f46
Gianni16 <55253221+Gianni16@users.noreply.github.com>
2019-12-13 18:32:00 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function getHeaderPortfolio () {
+  let element = document.getElementById('display');
+  element.style.backgroundImage = 'linear-gradient(to right, #ff6193, #a3beff)';
+}
+
+function getHeaderAbout () {
+  let element = document.getElementById('display');
+  element.style.backgroundImage = 'linear-gradient(to left, #ff6193, #e6f587)';
+}
